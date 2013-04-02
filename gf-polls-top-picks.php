e81cf1bd8776849b66eaafb01e7e1f4af24da7ce
Roy Sivan <roysivan@RoyMacbook.local>
2013-04-02 16:17:22 -0700

    GNU License

--- gf-polls-top-picks.php
+++ gf-polls-top-picks.php
@@ -6,6 +6,20 @@ Description: A way to grab the Top 5, 10, etc. picks of your GF Poll
 Author: Roy Sivan
 Version: 1.0
 Author URI: http://www.roysivan.com/
+/***************  LICENSE   **********************
+This plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ******************************************/
 
 function GFTop($atts){
